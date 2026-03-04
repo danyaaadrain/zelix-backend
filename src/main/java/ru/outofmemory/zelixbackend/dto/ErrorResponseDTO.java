4157afddacb54fc7e7d922514ca48e6a9203d190
Daniil <danber005@yandex.ru>
2026-03-04 19:24:03 +0500

    Рефактор

--- src/main/java/ru/outofmemory/zelixbackend/dto/ErrorResponseDTO.java
+++ src/main/java/ru/outofmemory/zelixbackend/dto/ErrorResponseDTO.java
@@ -1,10 +1,12 @@
 package ru.outofmemory.zelixbackend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResponseDTO {
     private String message;
     private int status;
